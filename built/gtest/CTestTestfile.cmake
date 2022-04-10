65526f6008217f4c1f1130d87533e6bbf2b29d27
Kyala16 <91627800+Kyala16@users.noreply.github.com>
2022-04-10 18:43:53 +0300

    Delete built directory

--- built/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyala16/lab06/gtest
-# Build directory: /home/kyala16/lab06/built/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- built/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyala16/lab06/gtest/googletest
-# Build directory: /home/kyala16/lab06/built/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
