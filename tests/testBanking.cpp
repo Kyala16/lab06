9af348674e5b481963208d66ac422e6c8306b477
Kyala16 <91627800+Kyala16@users.noreply.github.com>
2022-04-12 17:13:21 +0300

    Update testBanking.cpp

--- tests/testBanking.cpp
+++ tests/testBanking.cpp
@@ -2,6 +2,13 @@
 #include <Account.h>
 #include <Transaction.h>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+struct MockTrans : public Transaction{
+    MOCK_METHOD3(Make, bool(Account& from, Account& to, int sum));
+    MOCK_METHOD1(set_fee, void(int fee));
+    MOCK_METHOD0(fee, int());
+};
 
 TEST(Account, Lock){
 	Account store_bank(105, 150000);
@@ -39,6 +46,37 @@ TEST(Account, ChangeBalance){
 
 	store_bank.Unlock();
 	car_bank.Unlock();
+}
+
+TEST(Transaction, TransMockTest1){
+    MockTrans Acc_Trans1;
+    MockAcc Human1(1, 10000);
+    MockAcc Human2(2, 230000);
+    MockAcc Car_Shop(3, 8000000);
+    MockAcc Organisation(4, 5500);
+    EXPECT_CALL(Acc_Trans1, set_fee(testing::_)).Times(1);
+    EXPECT_CALL(Acc_Trans1, Make(testing::_, testing::_, testing::_)).Times(2);
+    EXPECT_CALL(Acc_Trans1, fee()).Times(1);
+    EXPECT_CALL(Human1, GetBalance()).Times(1);
+    EXPECT_CALL(Human2, GetBalance()).Times(1);
+    Acc_Trans1.set_fee(500);
+    Acc_Trans1.Make(Human1, Human2, 2000);
+    Acc_Trans1.fee();
+    Human1.GetBalance();
+    Human2.GetBalance();
+    Acc_Trans1.Make(Organisation, Human2, 20000);
+}
+
+TEST(Transaction, TransMockTest2){
+    MockTrans Acc_Trans2;
+    MockAcc Human1(1, 10000);
+    MockAcc Human2(2, 230000);
+    MockAcc Car_Shop(3, 8000000);
+    MockAcc Organisation(4, 5500);
+    EXPECT_CALL(Acc_Trans2, Make(testing::_, testing::_, testing::_)).Times(3);
+    Acc_Trans2.Make(Car_Shop, Car_Shop, 100000);
+    Acc_Trans2.Make(Car_Shop, Organisation, -5600);
+    Acc_Trans2.Make(Human2, Car_Shop, 55);
 }
 
 TEST(Transaction, Throws){
